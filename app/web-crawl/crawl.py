cb41af54129a11b4cbd9e853c38337c7c29e4a46
Jiho Joo <aksjajsxm123@gmail.com>
2023-06-29 23:21:46 +0900

    [추가]나무위키 디시인사이드/밈 카테고리 크롤링 세팅

--- app/web-crawl/crawl.py
+++ app/web-crawl/crawl.py
@@ -1,8 +1,18 @@
+import requests as req
+
 from bs4 import BeautifulSoup
 
-soup = BeautifulSoup("<p>Some<b>bad<i>HTML")
 
-print(soup.prettify())
+bad_words: ['대통령','고인','노무현','이명박', '박근혜']
 
-print(soup.b)
-print(soup.find(text="bad"))
+
+url = 'https://namu.wiki/w/%EB%B6%84%EB%A5%98:%EB%94%94%EC%8B%9C%EC%9D%B8%EC%82%AC%EC%9D%B4%EB%93%9C/%EB%B0%88'
+
+res = req.get(url, headers={'User-Agent': 'Mozilla/5.0'})
+
+if res.status_code == 200:
+    html = res.text
+    soup = BeautifulSoup(html, 'lxml')
+    print(soup)
+else:
+    print(res)
