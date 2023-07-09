946d7ee67a94833836a7a95b9124645182133644
Jiho Joo <aksjajsxm123@gmail.com>
2023-07-09 23:53:37 +0900

    [추가]postgresql 클래스 추가

--- app/db/db.py
+++ app/db/db.py
@@ -1,11 +1,24 @@
 import psycopg2
-import os
 
-conn = psycopg2.connect(os.getenv('POSTGRES_CONNECTION'))
 
-cur = conn.cursor()
+class PostgreSQL:
+    def __init__(self,connectioninfo:str):
+        self.connectionInfo = connectioninfo
 
-cur.execute("select * from test")
+        try:
+            self.conn = psycopg2.connect(self.connectionInfo)
+        except Exception as e:
+            raise e
+        else:
+            self.cur = self.conn.cursor()
 
-result = cur.fetchone();
-print(result);
+
+    def insertData(self, llm:str, dataList:list):
+        for data in dataList:
+            self.cur.execute(f'insert into llm_buzzwords (name, descr, tags, llm) values ("{data.name}","{data.descr}","{data.tags}","{llm}")')
+
+    def getRecentData(self, lastId:int)->list:
+        self.cur.execute(f'select * from llm_buzzwords where id > {lastId}')
+        return self.cur.fetchall()
+    def closeConnection(self):
+        self.conn.close()
