6906f38f79fc9b91857aa47937ecc824d7edccb0
Jiho Joo <aksjajsxm123@gmail.com>
2023-07-10 20:14:31 +0900

    [수정]router 분리, import하여 사용으로 수정

--- app/main.py
+++ app/main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
+from app.routers import router
+import uvicorn
 
 
 app = FastAPI()
+app.include_router(router.mainRouter)
 
-@app.get('/')
-def hello():
-    return "hi!"
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
